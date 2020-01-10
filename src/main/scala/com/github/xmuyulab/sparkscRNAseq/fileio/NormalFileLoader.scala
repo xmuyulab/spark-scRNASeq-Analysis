7c7c24954c5a59ca3ec02c11ef30c9c0489e1dd6
6liuyu123 <1272635425@qq.com>
2020-01-10 11:48:53 +0800

    version 0.0.1

--- src/main/scala/com/github/xmuyulab/sparkscRNAseq/fileio/NormalFileLoader.scala
+++ src/main/scala/com/github/xmuyulab/sparkscRNAseq/fileio/NormalFileLoader.scala
@@ -3,10 +3,12 @@
  */
 package com.github.xmuyulab.sparkscRNAseq.fileio
 
-import com.github.xmuyulab.sparkscRNAseq.data.basic.FastqRecord
 import org.apache.hadoop.io.Text
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+
+import com.github.xmuyulab.sparkscRNAseq.data.basic.FastqRecord
+import com.github.xmuyulab.sparkscRNAseq.fileio.format.FastqR1InputFormat
 
 class NormalFileLoader extends FileLoader{
 
@@ -21,11 +23,16 @@ class NormalFileLoader extends FileLoader{
   }
 
   override def loadFastqR1ToRdd(sc:SparkContext,filePath:String):RDD[FastqRecord]={
-    val records=sc.textFile(filePath)
+    val records=sc.newAPIHadoopFile(      //newAPIHadoopFile(path,format,key,value)
+      transFilePath(filePath),
+      classOf[FastqR1InputFormat],
+      classOf[Void],
+      classOf[Text]
+    )
 
     val compressFlagValue=sc.broadcast(true).value
     records.map(record=>{
-      val strIter
+      val strIter=StringUtils
     })
   }
 
