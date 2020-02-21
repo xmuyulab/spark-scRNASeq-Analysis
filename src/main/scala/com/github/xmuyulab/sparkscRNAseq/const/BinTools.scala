d7fd52064b556a425de5465fafa6755dedea6fdd
6liuyu123 <1272635425@qq.com>
2020-02-21 22:49:38 +0800

    BinTools.scala.1 commit

--- src/main/scala/com/github/xmuyulab/sparkscRNAseq/const/BinTools.scala
+++ src/main/scala/com/github/xmuyulab/sparkscRNAseq/const/BinTools.scala
@@ -4,8 +4,28 @@
 package com.github.xmuyulab.sparkscRNAseq.const
 
 import java.io.FileInputStream
-import java.util
+import java.util.Properties
+
+import com.github.xmuyulab.sparkscRNAseq.utils.FileUtils
 
 object BinTools {
+  val binDirPath=FileUtils.join(FileUtils.getDirPathNoEndSeparator(
+    FileUtils.getDirPathNoEndSeparator(
+      FileUtils.getDirPathNoEndSeparator(
+        FileUtils.getDirPathNoEndSeparator(
+          FileUtils.getDirPathNoEndSeparator(
+            FileUtils.getDirPathNoEndSeparator(
+              FileUtils.getDirPathNoEndSeparator(
+                this.getClass().getResource("").getPath()))))))),"bin")
 
+  val confPath={
+    val tmpPath=FileUtils.join(binDirPath,"config.properties")
+    if(tmpPath.startsWith("file:")) tmpPath.substring(5) else tmpPath
+  }
+
+  val processOptimize={
+    val properties=new Properties()
+    properties.load(new FileInputStream(confPath))
+    properties.getProperty("processOptimize").toBoolean
+  }
 }
