41ba9715271925e4b88fcd5a013a3eaa86ade6ce
6liuyu123 <1272635425@qq.com>
2020-03-09 12:28:50 +0800

    NormalFileLoader.scala commit

---
+++ src/main/java/com/github/xmuyulab/sparkscRNAseq/fileio/format/FastqR2InputFormat.java
@@ -0,0 +1,11 @@
+package com.github.xmuyulab.sparkscRNAseq.fileio.format;
+
+/**
+ * This is Description
+ *
+ * @author liuyu
+ * @date 2020/03/09
+ */
+public class FastqR2InputFormat {
+
+}
--- src/main/scala/com/github/xmuyulab/sparkscRNAseq/fileio/NormalFileLoader.scala
+++ src/main/scala/com/github/xmuyulab/sparkscRNAseq/fileio/NormalFileLoader.scala
@@ -5,12 +5,13 @@ package com.github.xmuyulab.sparkscRNAseq.fileio
 
 import org.apache.hadoop.io.Text
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
-
 import com.github.xmuyulab.sparkscRNAseq.data.basic.FastqRecord
 import com.github.xmuyulab.sparkscRNAseq.fileio.format.FastqR1InputFormat
+import com.github.xmuyulab.sparkscRNAseq.fileio.format.FastqR2InputFormat
 
-class NormalFileLoader extends FileLoader{
+object NormalFileLoader extends FileLoader{
 
   def transFilePath(filePath:String):String= {
     if (filePath.startsWith("hdfs://")) {
@@ -22,18 +23,41 @@ class NormalFileLoader extends FileLoader{
     }
   }
 
-  override def loadFastqR1ToRdd(sc:SparkContext,filePath:String):RDD[FastqRecord]={
-    val records=sc.newAPIHadoopFile(      //newAPIHadoopFile(path,format,key,value)
-      transFilePath(filePath),
-      classOf[FastqR1InputFormat],
-      classOf[Void],
-      classOf[Text]
+  //[K,V,F<:NewInputFormat[K,V]],return RDD[(K,V)]
+  override def loadFastqR1ToRdd(sc:SparkContext,filePath:String):RDD[(Text,Text)]={
+    val records=sc.newAPIHadoopFile(
+      transFilePath(filePath),            //path
+      classOf[FastqR1InputFormat],        //F
+      classOf[Text],                      //K
+      classOf[Text]                       //V
     )
+    records
+  }
 
-    val compressFlagValue=sc.broadcast(true).value
-    records.map(record=>{
-      val strIter=StringUtils
-    })
+  override def loadFastqR2ToRdd(sc: SparkContext, filePath: String): RDD[(Text, Text)] = {
+    val records=sc.newAPIHadoopFile(
+      transFilePath(filePath),            //path
+      classOf[FastqR2InputFormat],        //F
+      classOf[Text],                      //K
+      classOf[Text]                       //V
+    )
+    records
+  }
+
+  def loadFastqPairToRdd(sc:SparkContext,
+                                  filePath1:String,
+                                  filePath2:String):RDD[(String,(String,String,Int))]={
+    val fastqR1Rdd=loadFastqR1ToRdd(sc,filePath1)
+    val fastqR2Rdd=loadFastqR2ToRdd(sc,filePath2)
+    val whitelist=sc.makeRDD(fastqR1Rdd.map(line=>(line._1,1))
+                            .reduceByKey((a,b)=>a+b)
+                            .sortBy(_._2,false)
+                            .take(100))
+    fastqR1Rdd.join(whitelist)
+              .map(line=>
+                    (new Text(line._2._1.toString().substring(10,line._2._1.toString().length()-16)),(line._1.toString()+line._2._1.toString().substring(0,10),line._2._2)))
+              .join(fastqR2Rdd)
+              .map(line=>(line._1.toString(),(line._2._1._1,line._2._2.toString(),line._2._1._2)))
   }
 
 }
