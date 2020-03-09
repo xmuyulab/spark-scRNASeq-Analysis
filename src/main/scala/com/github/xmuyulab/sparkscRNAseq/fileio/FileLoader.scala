b9a0b75465e8eb9988094bdc3908aecbe88ed538
6liuyu123 <1272635425@qq.com>
2020-03-09 12:31:27 +0800

    FileLoader.scala commit

--- src/main/scala/com/github/xmuyulab/sparkscRNAseq/fileio/FileLoader.scala
+++ src/main/scala/com/github/xmuyulab/sparkscRNAseq/fileio/FileLoader.scala
@@ -1,5 +1,16 @@
 package com.github.xmuyulab.sparkscRNAseq.fileio
 
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.hadoop.io.Text
+
 trait FileLoader {
 
+  def loadFastqR1ToRdd(sc:SparkContext,filePath:String):RDD[(Text,Text)];
+
+  def loadFastqR2ToRdd(sc:SparkContext,filePath:String):RDD[(Text,Text)];
+
+  def loadFastqPairToRdd(sc:SparkContext,
+                         filePath1:String,
+                         filePath2:String):RDD[(String,(String,String,Int))]
 }
