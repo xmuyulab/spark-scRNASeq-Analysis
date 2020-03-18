717cf99b029552b81aee07a495fac16fff29161f
6liuyu123 <1272635425@qq.com>
2020-03-18 17:52:40 +0800

    JniParameters.java.0 commit

--- src/main/java/com/github/xmuyulab/sparkscRNAseq/jstar/JniParameters.java
+++ src/main/java/com/github/xmuyulab/sparkscRNAseq/jstar/JniParameters.java
@@ -12,7 +12,18 @@ public class JniParameters {
 
   private native long createNativeParameters();
 
+  private native void ParametersInputParameters(long address,int argInN,char[][] argIn);
+
   public JniParameters(){
     nativeParameters=createNativeParameters();
+    char[][] myParameters=new char[6][];
+    for(int i=0;i<6;i++){
+      myParameters[i]=new char[6];
+      for(int j=0;j<6;j++){
+        myParameters[i][j]='a';
+      }
+    }
+    ParametersInputParameters(nativeParameters,6,myParameters);
   }
 }
+
